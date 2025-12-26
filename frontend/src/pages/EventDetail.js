7939932bd5b5983a80cde1a28ae1224e8c06e180
Alexander Novitsky <novitsky.413x@gmail.com>
2025-12-26 16:23:18 +0300

    fixed errors related to outdated version of react-router-dom (json() and defer() removed)

--- frontend/src/pages/EventDetail.js
+++ frontend/src/pages/EventDetail.js
@@ -1,94 +1,65 @@
 import { Suspense } from 'react';
-import {
-  useRouteLoaderData,
-  json,
-  redirect,
-  defer,
-  Await,
-} from 'react-router-dom';
-
+import { redirect, useRouteLoaderData, Await } from 'react-router-dom';
 import EventItem from '../components/EventItem';
 import EventsList from '../components/EventsList';
 
 function EventDetailPage() {
-  const { event, events } = useRouteLoaderData('event-detail');
-
-  return (
-    <>
-      <Suspense fallback={<p style={{ textAlign: 'center' }}>Loading...</p>}>
-        <Await resolve={event}>
-          {(loadedEvent) => <EventItem event={loadedEvent} />}
-        </Await>
-      </Suspense>
-      <Suspense fallback={<p style={{ textAlign: 'center' }}>Loading...</p>}>
-        <Await resolve={events}>
-          {(loadedEvents) => <EventsList events={loadedEvents} />}
-        </Await>
-      </Suspense>
-    </>
-  );
+    const { event, events } = useRouteLoaderData('event-detail');
+    return (
+        <>
+            <Suspense fallback={<p style={{ textAlign: 'center' }}>Loading event...</p>}>
+                <Await resolve={event}>{(loadedEvent) => <EventItem event={loadedEvent} />}</Await>
+            </Suspense>
+            <Suspense fallback={<p style={{ textAlign: 'center' }}>Loading events...</p>}>
+                <Await resolve={events}>{(loadedEvents) => <EventsList events={loadedEvents} />}</Await>
+            </Suspense>
+        </>
+    );
 }
-
 export default EventDetailPage;
 
 async function loadEvent(id) {
-  const response = await fetch('http://localhost:8080/events/' + id);
-
-  if (!response.ok) {
-    throw json(
-      { message: 'Could not fetch details for selected event.' },
-      {
-        status: 500,
-      }
-    );
-  } else {
-    const resData = await response.json();
-    return resData.event;
-  }
+    const response = await fetch('http://localhost:8080/events/' + id);
+    if (!response.ok) {
+        throw new Response(JSON.stringify({ message: 'Could not fetch details for selected event.' }), {
+            status: 500,
+        });
+    } else {
+        const resData = await response.json();
+        return resData.event;
+    }
 }
 
 async function loadEvents() {
-  const response = await fetch('http://localhost:8080/events');
-
-  if (!response.ok) {
-    // return { isError: true, message: 'Could not fetch events.' };
-    // throw new Response(JSON.stringify({ message: 'Could not fetch events.' }), {
-    //   status: 500,
-    // });
-    throw json(
-      { message: 'Could not fetch events.' },
-      {
-        status: 500,
-      }
-    );
-  } else {
-    const resData = await response.json();
-    return resData.events;
-  }
+    const response = await fetch('http://localhost:8080/events');
+    if (!response.ok) {
+        // return { isError: true, message: 'Could not fetch events.' };
+        throw new Response(JSON.stringify({ message: 'Could not fetch events.' }), { status: 500 });
+    } else {
+        const resData = await response.json();
+        return resData.events;
+    }
 }
 
 export async function loader({ request, params }) {
-  const id = params.eventId;
-
-  return defer({
-    event: await loadEvent(id),
-    events: loadEvents(),
-  });
+    const id = params.eventId;
+    return {
+        event: await loadEvent(id), // waits for the event to load before rendering the page
+        events: loadEvents(), // will load events after the page has been rendered
+        /* thus 'await' is a switch to control which data should be awaited before rendering/moving to the page,
+        and which data should be deferred (load the data after moving to the page) */
+    };
 }
 
-export async function action({ params, request }) {
-  const eventId = params.eventId;
-  const response = await fetch('http://localhost:8080/events/' + eventId, {
-    method: request.method,
-  });
-
-  if (!response.ok) {
-    throw json(
-      { message: 'Could not delete event.' },
-      {
-        status: 500,
-      }
-    );
-  }
-  return redirect('/events');
+export async function action({ request, params }) {
+    const eventId = params.eventId;
+    const response = await fetch('http://localhost:8080/events/' + eventId, {
+        method: request.method,
+    });
+    if (!response.ok) {
+        throw new Response(JSON.stringify({ message: 'Could not delete event.' }), {
+            status: 500,
+        });
+    }
+    return redirect('/events');
 }
--- frontend/src/pages/Root.js
+++ frontend/src/pages/Root.js
@@ -1,19 +1,19 @@
-import { Outlet, useNavigation } from 'react-router-dom';
+import { Outlet } from 'react-router-dom';
 
 import MainNavigation from '../components/MainNavigation';
 
 function RootLayout() {
-  // const navigation = useNavigation();
+    // const navigation = useNavigation();
 
-  return (
-    <>
-      <MainNavigation />
-      <main>
-        {/* {navigation.state === 'loading' && <p>Loading...</p>} */}
-        <Outlet />
-      </main>
-    </>
-  );
+    return (
+        <>
+            <MainNavigation />
+            <main>
+                {/* {navigation.state === 'loading' && <p>Loading...</p>} */}
+                <Outlet />
+            </main>
+        </>
+    );
 }
 
 export default RootLayout;
