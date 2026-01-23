1f78b002936ffa02c009c16d1e5cc0ad710c3bda
Alexander Novitsky <novitsky.413x@gmail.com>
2026-01-23 15:36:51 +0300

    400. Managing the Token Expiration

--- frontend/src/pages/Root.js
+++ frontend/src/pages/Root.js
@@ -1,7 +1,7 @@
 import { Outlet, useLoaderData, useSubmit } from 'react-router-dom';
-
 import MainNavigation from '../components/MainNavigation';
 import { useEffect } from 'react';
+import { getTokenDuration } from '../util/auth';
 
 function RootLayout() {
     // const navigation = useNavigation();
@@ -11,12 +11,15 @@ function RootLayout() {
         if (!token) {
             return;
         }
-        setTimeout(
-            () => {
-                submit(null, { action: '/logout', method: 'post' });
-            },
-            1 * 60 * 60 * 1000,
-        );
+        if (token === 'EXPIRED') {
+            submit(null, { action: '/logout', method: 'post' });
+            return;
+        }
+        const tokenDuration = getTokenDuration();
+        console.log(tokenDuration);
+        setTimeout(() => {
+            submit(null, { action: '/logout', method: 'post' });
+        }, tokenDuration);
     }, [token, submit]);
 
     return (
